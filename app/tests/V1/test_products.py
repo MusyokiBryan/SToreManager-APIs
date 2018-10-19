0f168eefebc14b34ea78699782f7b91612b5ec96
Brian Musyoki <Musyokibr@gmail.com>
2018-10-19 19:14:34 +0300

    [Chore #161348834] writing of tests for the endpoints

---
+++ app/tests/V1/test_products.py
@@ -0,0 +1,26 @@
+import unittest
+import sys  # fix import errors
+import os
+from app.tests.V1.base import ConfigTestCase
+import json
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+class ProductTest(ConfigTestCase):
+    """this class contains Product tests"""
+
+    def test_get_products(self):
+        response = self.client().get()
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_a_product(self):
+        response = self.client().get()
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_product(self):
+        response = self.client().post( data=json.dumps(product), content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+
+
+if __name__ == '__main__':
+    unittest.main()
