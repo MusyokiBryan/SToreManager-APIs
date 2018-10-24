1bebef93ab5ee308793c131ce6f4ae75c9576fef
Brian Musyoki <Musyokibr@gmail.com>
2018-10-24 18:28:42 +0300

    [Bug #161452294] made fixes to files affecting travisCI builds and affecting badges, fixed tests importing wrong config files

--- app/api/V1/models.py
+++ app/api/V1/models.py
@@ -1,4 +1,4 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Products:
@@ -14,6 +14,11 @@ class Products:
             return {"txt": "Product not found"}
         return self.products
 
+    def get_a_product(self, product_id):
+        if self.products == {}:
+            return {"txt": "Product not found"}
+        return self.products[product_id]
+
     def create_product(self, product_name, product_price):
         new_id = len(self.products) + 1
         self.products[new_id] = {"product_name": product_name,
@@ -21,29 +26,20 @@ class Products:
         res = self.products[new_id]
         return {"msg": "Product added successfully", "data": res}
 
-    def get_a_product(self, product_id):
-        if self.products == {}:
-            return {"txt": "Product not found"}
-        return self.products[product_id]
-
-    def delete_a_product(self, product_id):
-        del self.products[product_id]
-        return {"txt": "Product Deleted"}
-
     def edit_product(self, product_id, product_name, product_price):
         self.products[product_id] = {"product_id": product_id, "product_name": product_name,
                                      "product_price": product_price}
         return {"msg": "Sale Edited"}
 
-    # product = Products()
-    # print(product.get_all_products())
-    # print(product.get_a_product(2))
-    # product.create_product("Socks", 456)
-    # print(product.products)
+    def delete_a_product(self, product_id):
+        if product_id not in self.products:
+            return {"txt": "product not found"}
+        del self.products[product_id]
+        return {"txt": "product Deleted"}
 
 
 class Sales:
-    """Functionality of products"""
+    """Functionality of sales"""
     sales = {}
 
     def see_sales(self):
@@ -51,22 +47,26 @@ class Sales:
             return {"txt": "No sales added."}
         return self.sales
 
+    def get_a_sale(self, sale_id):
+        if self.sales == {} or sale_id not in self.sales:
+            return {"txt": "Sale not found."}
+        return self.sales[sale_id]
+
     def post_a_sale(self, product_name, number, sell_price):
         new_id = len(self.sales) + 1
         self.sales[new_id] = {"product_name": product_name, "number": number,
                               "sell_price": sell_price}
         return {"msg": "added successfully"}
 
-    def get_a_sale(self, sale_id):
-        if self.sales == {} or sale_id not in self.sales:
-            return {"txt": "No sales added."}
-        return self.sales[sale_id]
-
     def edit_sale(self, sale_id, product_name, number, sell_price):
+        if self.sales == {} or sale_id not in self.sales:
+            return {"txt": "Sale not found."}
         self.sales[sale_id] = {"product_name": product_name, "number": number, "sell_price": sell_price}
         return {"msg": "Sale Edited"}
 
     def delete_a_sale(self, sale_id):
+        if sale_id not in self.sales:
+            return {"txt": "sale not found"}
         del self.sales[sale_id]
         return {"txt": "sale Deleted"}
 
@@ -79,6 +79,18 @@ class Users:
         self.users[user_name] = {"email": email, "password": hidden}
         res = self.users[user_name]
         return {"msg": "user added successfully", "data": res}
+
+    def login(self, user_name, password):
+        if user_name in self.users:
+            if check_password_hash(self.users[user_name]["password"], password=password):
+                print("Logged In")
+                return {"txt": "Successfully logged In"}
+            else:
+                print("Invalid Password")
+                return {"txt": "Invalid Password"}
+        else:
+            print("Invalid Username")
+            return {"txt": "Invalid Username"}
 
     def get_users(self):
         if self.users == {}:
--- app/tests/V1/test_sales.py
+++ app/tests/V1/test_sales.py
@@ -1,18 +1,12 @@
 import unittest
-import sys  # fix import errors
-import os
 from app.tests.V1.base import ConfigTestCase
 import json
-
-
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 class SalesTest(ConfigTestCase):
     """this class contains Product tests"""
 
     def test_see_sales(self):
-
         response = self.client().get("/api/v1/sales")
         self.assertEqual(response.status_code, 200)
 
@@ -22,8 +16,7 @@ class SalesTest(ConfigTestCase):
 
     def test_post_a_sale(self):
         sale = {"product_name": "shirts", "number": 5500, "sell_price": 500}
-        response = self.client().post("/api/v1/sales", data=json.dumps(sale),
-                                      content_type="application/json")
+        response = self.client().post("/api/v1/sales", data=json.dumps(sale), content_type="application/json")
         self.assertEqual(response.status_code, 201)
 
 
